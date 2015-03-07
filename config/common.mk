45b26b60f6bb3e21d53f1d879034b07bf3801e67
Chang Long <changlon@gmail.com>
2015-03-07 10:11:40 +0100

    add prebuild mdnsd

--- config/common.mk
+++ config/common.mk
@@ -31,7 +31,8 @@ PRODUCT_COPY_FILES += \
     vendor/slim/prebuilt/common/bin/50-slim.sh:system/addon.d/50-slim.sh \
     vendor/slim/prebuilt/common/bin/99-backup.sh:system/addon.d/99-backup.sh \
     vendor/slim/prebuilt/common/etc/backup.conf:system/etc/backup.conf \
-    vendor/slim/prebuilt/common/app/NovaLauncher.apk:system/app/NovaLauncher/NovaLauncher.apk
+    vendor/slim/prebuilt/common/app/NovaLauncher.apk:system/app/NovaLauncher/NovaLauncher.apk \
+    vendor/slim/prebuilt/common/bin/mdnsd:system/bin/mdnsd
 
 # Signature compatibility validation
 PRODUCT_COPY_FILES += \
